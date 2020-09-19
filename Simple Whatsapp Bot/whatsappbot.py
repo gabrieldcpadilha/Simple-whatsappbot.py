1b82a9284ef3860e1d0a41eb5a80b56562bd7f65
gabrielpadilha-saru <gdcpadilha@outlook.com>
2020-09-19 05:44:34 -0300

    Simple whatsappbot.py

--- Simple Whatsapp Bot/whatsappbot.py
+++ Simple Whatsapp Bot/whatsappbot.py
@@ -7,22 +7,22 @@ from selenium.webdriver.common.keys import Keys
 # navegar até o whatsapp web
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://web.whatsapp.com/')
-time.sleep(30)
+time.sleep(20)
 
 # definir contatos e mensagem a ser enviada
-contatos = ['Eliana Nogueira', 'Aníbal Padilha', 'Amor']
-mensagem = 'Teste N2: Testando script de envio de mensagens automáticas. :D :D :D'
+contatos = ['Aníbal Padilha', 'Bolinha Sarutaiá', 'Jean Capivara']
+mensagem = 'Aoba! (Y)'
 
 
 # campo de pesquisa 'copyable-text selectable-text' OBS: index[0]
 def buscar_contato(contato):
-    campo_pesquisa_contato = driver.find_element_by_xpath(
+    campo_pesquisa_contato = driver.find_elements_by_xpath(
         '//div[contains(@class,"copyable-text selectable-text")]'
     )
-    time.sleep(5)
-    campo_pesquisa_contato.click()
-    campo_pesquisa_contato.send_keys(contato)
-    campo_pesquisa_contato.send_keys(Keys.ENTER)
+    time.sleep(3)
+    campo_pesquisa_contato[0].click()
+    campo_pesquisa_contato[0].send_keys(contato)
+    campo_pesquisa_contato[0].send_keys(Keys.ENTER)
 
 
 # campo de mensagem privada 'copyable-text selectable-text' OBS: index[1]
@@ -30,7 +30,7 @@ def enviar_mensagem(mensagem):
     campo_pesquisa_mensagem = driver.find_elements_by_xpath(
         '//div[contains(@class,"copyable-text selectable-text")]'
     )
-    time.sleep(5)
+    time.sleep(2)
     campo_pesquisa_mensagem[1].click()
     campo_pesquisa_mensagem[1].send_keys(mensagem)
     campo_pesquisa_mensagem[1].send_keys(Keys.ENTER)
